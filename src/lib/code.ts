42112cbcc592bee0193de927d99e8f82186d288c
Hernan Alvarado <hernanvid123@gmail.com>
2024-12-02 18:59:00 -0500

    chore(docs): add `deploy` page sections includes
    - Install `vercel` as a global dependency
    - Link your project
    - Build your project

--- src/lib/code.ts
+++ src/lib/code.ts
@@ -1,4 +1,9 @@
 export const mdx = {
+    installation: `
+npm install @next/mdx @mdx-js/loader @mdx-js/react @types/mdx
+# or
+pnpm add @next/mdx @mdx-js/loader @mdx-js/react @types/mdx
+    `,
     nextConfig: `
 import type { NextConfig } from "next"
 import createMDX from "@next/mdx"
@@ -12,12 +17,30 @@ const withMDX = createMDX({
 })
 
 export default withMDX(nextConfig)
-`,
+    `,
     mdxComponents: `
 import type { MDXComponents } from "mdx/types"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return { ...components }
 }
-`,
+    `,
+}
+
+export const deploy = {
+    installation: `
+npm i -g vercel@latest
+# or
+pnpm add -g vercel@latest
+    `,
+    link: `
+vercel link
+ ? Set up "~\path" ?
+ ? Which scope should contain your project ?
+ ? Link to existing project ?
+ ? What's your project's name ?
+    `,
+    build: `
+vercel build
+    `,
 }
